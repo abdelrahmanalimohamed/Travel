8bedb74fe53c5320dabac8230b64902117b66c42
abdelrahmanalimohamed <abdelrahmanali.mohamed@outlook.com>
2022-05-16 13:39:24 +0200

    Add project files.

---
+++ src/infrastructure/Travel.Data/Context/TravelDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travel.Domain.Entities;
+
+namespace Travel.Data.Context
+{
+    public class TravelDbContext : DbContext
+    {
+
+        public TravelDbContext(DbContextOptions<TravelDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<TourList> TourLists { get; set; }
+        public DbSet<TourPackage> TourPackages { get; set; }
+
+
+    }
+}
