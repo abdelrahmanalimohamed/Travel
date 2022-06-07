fa29d4c46eea86f554ec5f6dc546a9c92f74852a
abdelrahmanalimohamed <abdelrahmanali.mohamed@outlook.com>
2022-06-08 01:30:19 +0200

    CQR and Mediator Pattern

---
+++ src/core/Travel.Application/Common/Interfaces/IDateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel.Application.Common.Interfaces
+{
+    internal interface IDateTime
+    {
+        DateTime NowUtc { get; }
+    }
+}
---
+++ src/core/Travel.Application/DTOS/TourLists/GetTours/ToursVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travel.Application.DTOS.Tour;
+
+namespace Travel.Application.DTOS.TourLists.GetTours
+{
+    public class ToursVm
+    {
+        public IList<TourListDto> Lists { get; set; }
+    }
+}
